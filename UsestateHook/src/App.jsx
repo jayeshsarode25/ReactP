efd02aff77d969754a5b3ce9dfff26b4b0df5c86
jayeshsarode25 <jayeshsarode2003@gmail.com>
2025-11-26 10:19:59 +0530

    feat: implement counter functionality with increment and decrement buttons

--- UsestateHook/src/App.jsx
+++ UsestateHook/src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+
+  const [num, setNum] = useState(0);
+
+  
+
+  const increment = () => {
+    setNum(num + 1);
+  }
+
+  const decrement = () => {
+    setNum(num - 1);
+  }
+
   return (
-    <div className='w-20 h-30'>
-      <h1 className='text-6xl'>0</h1>
-      <button>Increment</button>
-      <button>Decrement</button>
+    <div className='w-60 h-60 m-4 bg-gray-300 flex flex-col justify-center items-center gap-4'>
+      <h1 className='text-6xl bg-black text-white'>{num}</h1>
+      <button onClick={increment} className='bg-gray-600 text-white p-4 rounded-2xl'>Increment</button>
+      <button onClick={decrement} className='bg-gray-600 text-white p-4 rounded-2xl'>Decrement</button>
 
     </div>
   )
